a0a3a94e82205e4aaa8d8c1698d91c979dc09949
haitaochen <15262057819@163.com>
2016-10-14 14:38:34 +0800

    add cal&login

--- S01E15Relativelayout3/gen/com/ct/s01e15relativelayout3/R.java
+++ S01E15Relativelayout3/gen/com/ct/s01e15relativelayout3/R.java
@@ -24,11 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int cancelButton=0x7f080003;
         public static final int labelview=0x7f080000;
         public static final int okButton=0x7f080004;
         public static final int passwordText=0x7f080002;
+        public static final int resview=0x7f080005;
         public static final int usernameText=0x7f080001;
     }
     public static final class layout {
